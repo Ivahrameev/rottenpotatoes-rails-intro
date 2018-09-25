f2dbb582bb5e6e29483b8a4b9c29b247b4295934
Igor <igorvahrameev@gmail.com>
2018-09-25 02:18:41 +0000

    Part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    session[:sort]    = params[:sort]    if params[:sort]
+    
+    
+    if session[:sort] 
+        @movies = Movie.all.order(session[:sort])
+    else 
+        @movies = Movie.all
+    end
+    
+    if session[:sort] != params[:sort] 
+      redirect_to movies_path(sort: session[:sort])
+    end
+    
   end
 
   def new
